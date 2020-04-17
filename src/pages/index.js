289c77bc50191afbe220e9a3567780dcd42c60c9
nuttyears <whatiwannasayis@gmail.com>
2020-04-16 23:08:32 -0700

    lesson 2 finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,18 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <div>
     <SEO title="Home" />
-    <h1>Learn to design and code react apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code react apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <p>Now go build something great.</p>
+        <a href="/page-2/">Watch the video</a>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2 with the video</Link>
-  </Layout>
+    
+  </div>
 )
 
 export default IndexPage
